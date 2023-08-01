70fa24944893eea3d1413776119f5181c41afa51
hongzhang.yu <15216142871@163.com>
2023-08-01 18:14:50 +0800

    add aurora as

---
+++ aurora-as/src/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, const char *argv[]) {
+    if (argc < 2) {
+        printf("useage: ./aurora-as asm_file\n");
+        exit(-1);
+    }
+
+    std::ifstream fs(argv[1]);
+    if (!fs.is_open()) {
+        printf("open file failed\n");
+        exit(-1);
+    }
+
+    
+
+    std::cout << "hello aurora-as" << std::endl;
+    return 0;
+}
